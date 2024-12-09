068af03edf9f8af519bd82cbfc0fd0d0badee9c3
Idoko Innocent <iviidev899@gmail.com>
2024-12-09 05:50:47 +0100

    Create python script for calculating simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+def calc_simple_interest(principle, rate, time):
+    simple_interest = principle * rate * time
+    print(f'The simple interest is: {simple_interest}')
+
+
+calc_simple_interest(1000, 0.05, 3)
