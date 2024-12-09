e0adc107468eb9a81434d6987c18f44d572df9ad
Idoko Innocent <iviidev899@gmail.com>
2024-12-09 06:00:23 +0100

    Change code from functional to procedural

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,13 @@
-def calc_simple_interest(principal, rate, time):
-    interest = principal * rate * time
-    print(f'The simple interest is: {interest}')
+# def calc_simple_interest(principal, rate, time):
+#     interest = principal * rate * time
+#     print(f'The simple interest is: {interest}')
 
 
-calc_simple_interest(1000, 0.05, 3)
+# calc_simple_interest(1000, 0.05, 3)
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+print(f'The simple interest is: {interest}')
