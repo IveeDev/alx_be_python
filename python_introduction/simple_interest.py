4a103b7a612c6a20b663caeedc16f1c9a83b94a2
Idoko Innocent <iviidev899@gmail.com>
2024-12-09 05:54:16 +0100

    Change variable from priciple to principal and simple_interest to interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
-def calc_simple_interest(principle, rate, time):
-    simple_interest = principle * rate * time
-    print(f'The simple interest is: {simple_interest}')
+def calc_simple_interest(principal, rate, time):
+    interest = principal * rate * time
+    print(f'The simple interest is: {interest}')
 
 
 calc_simple_interest(1000, 0.05, 3)
