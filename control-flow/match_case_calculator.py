6fd949de2d586bf8189caf06479bdc96259cfaf5
Idoko Innocent <iviidev899@gmail.com>
2024-12-16 05:53:37 +0100

    Ensure consistent punctuation in print statements

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -6,15 +6,15 @@ arith_operations = input("Choose the operation (+, -, *, /): ")
 
 match arith_operations:
     case "+":
-        print(f"The result is {num1 + num2}")
+        print(f"The result is {num1 + num2}.")
     case "-":
-        print(f"The result is {num1 - num2}")
+        print(f"The result is {num1 - num2}.")
     case "*":
-        print(f"The result is {num1 * num2}")
+        print(f"The result is {num1 * num2}.")
     case "/":
         if num2 == 0:
             print("Cannot divide by zero.")
         else:
-            print(f"The result is {num1 / num2}")
+            print(f"The result is {num1 / num2}.")
     case _:
         print("Invalid operation")
