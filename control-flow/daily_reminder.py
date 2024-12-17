cd23f84dddb3b9bf4887b55a9ea10d7198f18d4f
Idoko Innocent <iviidev899@gmail.com>
2024-12-17 08:24:19 +0100

    Modify customize reminder message

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -9,7 +9,7 @@ match priority:
     case "high":
         reminder = f"Reminder: '{task}' is a high priority task"
     case "medium":
-        reminder = f"Reminder'{task}' is a medium priority task"
+        reminder = f"Note :'{task}' is a medium priority task"
     case "low":
         reminder = f"Note: '{task}' is a low priority task"
     case _:
