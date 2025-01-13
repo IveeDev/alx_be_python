9e4cdcaaa4ba59b05413608d68a8fa8e5ff28bf8
IveeDev <iviidev899@gmail.com>
2025-01-13 02:37:27 +0100

    Create a bank account script with withdraw, deposit and display_balance methods

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
+    
+    def deposit(self, amount):
+        """Add the specified amount to the account balance."""
+        if amount > 0:
+            self.account_balance = self.account_balance + amount
+        else:
+            print("Amount cannot be a negative value!")
+    def withdraw(self, amount):
+        """Deduct the specified amount from the account balance if funds are sufficient."""
+        if amount > 0:
+            if self.account_balance >= amount:
+                self.account_balance = self.account_balance - amount
+                return True
+            return False
+        return False
+    
+    def display_balance(self):
+        """Print the current balance in a user-friendly format."""
+        print(f"Current Balance: ${self.account_balance}")
